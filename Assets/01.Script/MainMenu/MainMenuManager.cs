6ab9430f63c3b6b6de86d2f6e3aa2b88d1f9086a
kangjiwon95 <wona2844@daum.net>
2024-05-07 20:31:42 +0900

    Revert "05.07"
    This reverts commit fa14a750f54078ce2539f1516cdf3f41864735ec.

--- Assets/01.Script/MainMenu/MainMenuManager.cs
+++ Assets/01.Script/MainMenu/MainMenuManager.cs
@@ -1,109 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
-    [Header("BakcImage")]
-    public Sprite[] backimages;
-    public Image backGround;
-
-    [Space(20)]
-    [Header("Button")]
-    public Button continueButton;
-    public Button newGameButton;
-    public Button optionButton;
-    public Button gamequitButton;
-    public Button yesButton;
-    public Button noButton;
-
-    [Space(20)]
-    [Header("Panel")]
-    public GameObject optionPanel;
-    public GameObject gameQuitPanel;
-
-
-    private void Start()
+    // Start is called before the first frame update
+    void Start()
     {
-        RandomBackImage();
+        
     }
 
-    private void Update()
+    // Update is called once per frame
+    void Update()
     {
-        OptionButton();
-        GameQuitButton();
-        NewGame();
-
+        
     }
-
-    #region MainScene에서 BackImage 랜덤 변경
-    private void RandomBackImage()
-    {
-        backGround.color = Color.white;
-        // 이미지 배열이 비어있지 않은지 확인
-        if (backimages.Length > 0)
-        {
-            // 랜덤으로 인덱스 선택
-            int randomIndex = Random.Range(0, backimages.Length);
-            // 선택한 이미지를 backGround의 sprite로 설정
-            backGround.sprite = backimages[randomIndex];
-        }
-        else
-        {
-            Debug.LogError("backimages 배열이 비어있습니다.");
-        }
-    }
-    #endregion
-
-    #region New Game
-    private void NewGame()
-    {
-        newGameButton.onClick.AddListener(() => SceneManager.LoadScene(1));
-    }
-    #endregion
-
-
-    #region OptionButton
-    private void OptionButton()
-    {
-        //람다식을 통해 함수 이벤트 호출
-        optionButton.onClick.AddListener(() => optionPanel.SetActive(true));
-
-        if(Input.GetButtonDown("Cancel"))
-        {
-            PanelOff(optionPanel);
-        }
-    }
-    #endregion
-
-    #region 게임종료 버튼
-    private void GameQuitButton()
-    {
-        gamequitButton.onClick.AddListener(() => gameQuitPanel.SetActive(true));
-
-        yesButton.onClick.AddListener(() => GameQuit());
-
-        noButton.onClick.AddListener(() => PanelOff(gameQuitPanel));
-    }
-
-    private void GameQuit()
-    {
-#if UNITY_EDITOR
-        // 에디터 상에서는 에디터를 종료합니다.
-        UnityEditor.EditorApplication.isPlaying = false;
-
-        // 빌드된 게임에서는 애플리케이션을 종료합니다.
-        Application.Quit();
-#endif
-    }
-    #endregion
-
-    #region 패널 종료
-    private void PanelOff(GameObject panel)
-    {
-        panel.SetActive(false);
-    }
-    #endregion
 }
